529be41d63232745028647dd09c26103f2f123ad
Maciej Jagiello <maciej.jagiello@codilime.com>
2019-03-09 22:46:05 +0100

    save to a file

--- src/pl/edu/pw/jagiello/rubikscube/model/state/ResultState.java
+++ src/pl/edu/pw/jagiello/rubikscube/model/state/ResultState.java
@@ -3,17 +3,26 @@ package pl.edu.pw.jagiello.rubikscube.model.state;
 import pl.edu.pw.jagiello.rubikscube.model.Model;
 import pl.edu.pw.jagiello.rubikscube.view.View;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
 
 public class ResultState extends State {
 
+  private final static String FILENAME = "results.csv";
   private Thread freeStatePostponed;
 
   public ResultState(final StateContext context, final Model model, final View view) {
     super(model, view);
     long timeDelta = view.stopTimer();
-    System.out.println(formatSeconds(timeDelta));
+
+    String result = formatSeconds(timeDelta);
+    appendStrToFile(result);
+    System.out.println(result);
 
     goFreeStateAfterPeriod(context, model, view, 1);
   }
@@ -38,5 +47,21 @@ public class ResultState extends State {
     double seconds = TimeUnit.NANOSECONDS.toMillis(timeLeft) / 1000.0;
     DecimalFormat df = new DecimalFormat("0.00");
     return df.format(seconds);
+  }
+
+  private void appendStrToFile(String str)
+  {
+    try {
+      BufferedWriter out = new BufferedWriter(
+          new FileWriter(FILENAME, true));
+      DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+      LocalDateTime now = LocalDateTime.now();
+      out.write(String.format("\"%s\",\"%s\"", str, dtf.format(now)));
+      out.newLine();
+      out.close();
+    }
+    catch (IOException e) {
+      System.out.println("exception occoured" + e);
+    }
   }
 }
